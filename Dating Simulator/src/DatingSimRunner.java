72e31a80fb3742d77532d65e12eba5791df455fc
joshua.obrecht <joshua.obrecht@MU-C1-11.mullen.pvt.k12.co.us>
2018-09-20 15:01:27 -0600

    started story

--- Dating Simulator/src/DatingSimRunner.java
+++ Dating Simulator/src/DatingSimRunner.java
@@ -5,10 +5,11 @@ import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
 public class DatingSimRunner
 	{
 		
-		public static void main(String[] args)
+		public static void main(String[] args) throws InterruptedException
 			{
-				CharacterCreator.createCharacters();
-				CharacterCreator.createPlayer();
+//				CharacterCreator.createCharacters();
+//				CharacterCreator.createPlayer();
+				GenerateStory.basicStory();
 
 			}
 		
---
+++ Dating Simulator/src/GenerateStory.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+public class GenerateStory
+	{
+		
+     public static void basicStory() throws InterruptedException
+     {
+    	System.out.println("Once again, ");
+    	TimeUnit.SECONDS.sleep(2); 
+    	System.out.print("you are the new kid in town.");
+    	
+     }
+     
+	}
