fec4b6736849e34bd1d12c123b38bd4a61c6c21a
joshua.obrecht <joshua.obrecht@MU-C1-11.mullen.pvt.k12.co.us>
2018-09-26 09:09:46 -0600

    added a static int scale to for time units to make debugging easier

--- Dating Simulator/src/GenerateStory.java
+++ Dating Simulator/src/GenerateStory.java
@@ -3,25 +3,26 @@ import java.util.concurrent.TimeUnit;
 public class GenerateStory
 	{
 	 static Scanner userInput= new Scanner(System.in);
+	 static int scale=1;
      public static void basicStory() throws InterruptedException
      {
     	
     	System.out.print(CharacterCreator.player.getName()+": 'Once again, ");
-    	TimeUnit.SECONDS.sleep((long) 1.5); 
+    	TimeUnit.SECONDS.sleep((long) ((long) scale*1.5)); 
     	System.out.println("I am the new kid in town...'");
     	System.out.println("(Whenever you see an elipse hit enter to advance the text on screen)");
     	userInput.nextLine();
 
     	System.out.println("'I have to move around a lot because of my dad's work.");
-    	TimeUnit.SECONDS.sleep((long) 1.5); 
+    	TimeUnit.SECONDS.sleep((long) ((long) scale*1.5)); 
     	System.out.println("This time, however, my dad was moved the weirdest place possible...'");
     	TimeUnit.SECONDS.sleep(1); 
     	userInput.nextLine();
     	
     	System.out.print("'The Ocean");
-    	TimeUnit.SECONDS.sleep(1); 
+    	TimeUnit.SECONDS.sleep(scale); 
     	System.out.print(" and not just some island in the middle of the ocean, ");
-    	TimeUnit.SECONDS.sleep((long) 2); 
+    	TimeUnit.SECONDS.sleep((long) ((long) scale*1.5)); 
     	System.out.println("he was LITERALLY moved underwater...'");
     	userInput.nextLine();
     	
@@ -29,7 +30,7 @@ public class GenerateStory
     	userInput.nextLine();
     	
     	System.out.print("'Except there's one catch, ");
-    	TimeUnit.SECONDS.sleep(2);
+    	TimeUnit.SECONDS.sleep(scale*2);
     	System.out.println("I'm the ONLY human (as far as I know)...'");
     	userInput.nextLine();
     	
